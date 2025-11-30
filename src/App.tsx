0fcf3481f1a28739393a29f978f697630afe2ac7
BuraYu <burak.yueksel@hotmail.com>
2025-11-30 21:14:04 +0100

    add: postcss and typography variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 const App = () => {
   return (
     <div className="flex">
-      <h1 className="text-preset-2">Notebook</h1>
+      <h1 className="text-preset-1-noto text-mint-500">Notebook</h1>
     </div>
   );
 };
